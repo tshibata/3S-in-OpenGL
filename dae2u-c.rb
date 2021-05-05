7f4475c1cc1051b239f91cc980ecc51f708f8d17
tshibata <staatsschreiber@gmail.com>
2021-05-05 13:43:17 +0900

    Fix handling collada file
    - Take care of transform
    - Extract only nodes specified
    - Upside down texture

--- dae2u-c.rb
+++ dae2u-c.rb
@@ -2,6 +2,7 @@ require 'rexml/document'
 
 $sourceMap = {}
 $verticesMap = {}
+$geometriesMap = {}
 
 class Source
 	attr_accessor :array
@@ -70,15 +71,47 @@ class Geometry
 	end
 end
 
-class Collada
-	attr_accessor :geometries
+class Node
+	attr_accessor :name, :transform, :geometry
 	def initialize(element)
-		@geometries = []
+		@name = element.attributes['name']
+		element.elements.each do |child|
+			if child.name == 'matrix'
+				if child.attributes['sid'] == 'transform'
+					@transform = child.text.to_s.split.map{ |s| s.to_f }
+				end
+			elsif child.name == 'instance_geometry'
+				@geometry = child.attributes['url']
+			end
+		end
+	end
+	def transform(v)
+		w = v[0] * @transform[12] +v[1] * @transform[13] + v[2] * @transform[14] + @transform[15];
+		v[0] = (v[0] * @transform[0] +v[1] * @transform[1] + v[2] * @transform[2] + @transform[3]) / w;
+		v[1] = (v[0] * @transform[4] +v[1] * @transform[5] + v[2] * @transform[6] + @transform[7]) / w;
+		v[2] = (v[0] * @transform[8] +v[1] * @transform[9] + v[2] * @transform[10] + @transform[11]) / w;
+	end
+end
+
+class Collada
+	attr_accessor :nodesMap
+	def initialize(element)
+		@nodesMap = {}
 		element.elements.each do |child|
 			if child.name == 'library_geometries'
 				child.elements.each do |child|
 					if child.name == 'geometry'
-						@geometries.push(Geometry.new(child))
+						$geometriesMap['#' + child.attributes['id']] = Geometry.new(child)
+					end
+				end
+			elsif child.name == 'library_visual_scenes'
+				child.elements.each do |child|
+					if child.name == 'visual_scene'
+						child.elements.each do |child|
+							if child.name == 'node'
+								@nodesMap[child.attributes['name']] = Node.new(child)
+							end
+						end
 					end
 				end
 			end
@@ -86,35 +119,38 @@ class Collada
 	end
 end
 
-ARGV.each do |src|
-	doc = REXML::Document.new(File.new(src))
-	collada = Collada.new(doc.root)
-	collada.geometries.each do |geometry|
-		name = File.join(File.dirname(src), geometry.name + '.u-c.txt')
-		File.open(name, 'w') do |o|
-			textureInput = geometry.triangles.inputs['TEXCOORD']
-			normalInput = geometry.triangles.inputs['NORMAL']
-			vertexInput = geometry.triangles.inputs['VERTEX']
-			textureSource = $sourceMap[textureInput.source]
-			normalSource = $sourceMap[normalInput.source]
-			vertexSource = $sourceMap[$verticesMap[vertexInput.source].inputs['POSITION'].source]
-			stride = geometry.triangles.p.length / (3 * geometry.triangles.count)
-			(0 ... geometry.triangles.count).each do |i|
-				(0 ... 3).reverse_each do |j|
-					k = geometry.triangles.p[stride * (i * 3 + j) + textureInput.offset]
-					o.write("u:#{textureSource.array[k * 2 + 0]}")
-					o.write(" v:#{textureSource.array[k * 2 + 1]}")
-					k = geometry.triangles.p[stride * (i * 3 + j) + vertexInput.offset]
-					o.write(" x:#{vertexSource.array[k * 3 + 0]}")
-					o.write(" y:#{vertexSource.array[k * 3 + 1]}")
-					o.write(" z:#{-vertexSource.array[k * 3 + 2]}")
-					k = geometry.triangles.p[stride * (i * 3 + j) + normalInput.offset]
-					o.write(" a:#{normalSource.array[k * 3 + 0]}")
-					o.write(" b:#{normalSource.array[k * 3 + 1]}")
-					o.write(" c:#{-normalSource.array[k * 3 + 2]}\n")
-				end
-				o.write("\n")
+src = ARGV.shift
+doc = REXML::Document.new(File.new(src))
+collada = Collada.new(doc.root)
+ARGV.each do |name|
+	node = collada.nodesMap[name]
+	geometry = $geometriesMap[node.geometry]
+	path = File.join(File.dirname(src), name + '.u-c.txt')
+	File.open(path, 'w') do |w|
+		textureInput = geometry.triangles.inputs['TEXCOORD']
+		normalInput = geometry.triangles.inputs['NORMAL']
+		vertexInput = geometry.triangles.inputs['VERTEX']
+		textureSource = $sourceMap[textureInput.source]
+		normalSource = $sourceMap[normalInput.source]
+		vertexSource = $sourceMap[$verticesMap[vertexInput.source].inputs['POSITION'].source]
+		stride = geometry.triangles.p.length / (3 * geometry.triangles.count)
+		(0 ... geometry.triangles.count).each do |i|
+			(0 ... 3).reverse_each do |j|
+				k = geometry.triangles.p[stride * (i * 3 + j) + textureInput.offset]
+				w.write("u:#{textureSource.array[k * 2 + 0]}")
+				w.write(" v:#{-textureSource.array[k * 2 + 1]}")
+				k = geometry.triangles.p[stride * (i * 3 + j) + vertexInput.offset]
+				v = vertexSource.array[k * 3, 3]
+				node.transform(v)
+				w.write(" x:#{v[0]}")
+				w.write(" y:#{v[1]}")
+				w.write(" z:#{-v[2]}")
+				k = geometry.triangles.p[stride * (i * 3 + j) + normalInput.offset]
+				w.write(" a:#{normalSource.array[k * 3 + 0]}")
+				w.write(" b:#{normalSource.array[k * 3 + 1]}")
+				w.write(" c:#{-normalSource.array[k * 3 + 2]}\n")
 			end
+			w.write("\n")
 		end
 	end
 end
