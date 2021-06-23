7a11bc1fa23a78edbd557a2c24ca582c73163c46
tshibata <staatsschreiber@gmail.com>
2021-06-24 00:12:34 +0900

    Optimize renderer

--- renderer.h
+++ renderer.h
@@ -3,7 +3,9 @@ extern GLuint * vbo;
 extern GLsizei * vbSize;
 extern GLuint * tex;
 
-void setPresence(AbstractPresence * p);
+inline void setPresence(AbstractPresence * p)
+{
+}
 
 template <class Head, class... Tail> void setPresence(AbstractPresence * p, Head&& head, Tail&&... tail)
 {
@@ -28,8 +30,16 @@ class DirectUniform
 private:
 	GLint location;
 public:
-	DirectUniform(GLuint program, const GLchar *name);
-	void set(AbstractPresence * b);
+	DirectUniform(GLuint program, const GLchar *name)
+	{
+		location = glGetUniformLocation(program, name);
+	}
+	void set(AbstractPresence * b)
+	{
+		Matrix4x4 objectMatrix;
+		b->getMatrix(objectMatrix.elements);
+		glUniformMatrix4fv(location, 1, GL_FALSE, objectMatrix.elements);
+	}
 };
 
 class UniformMatrix
@@ -38,8 +48,18 @@ private:
 	GLint location;
 	float * matrix;
 public:
-	UniformMatrix(GLuint program, const GLchar *name, float * matrix);
-	void set(AbstractPresence * b);
+	UniformMatrix(GLuint program, const GLchar *name, float * matrix) : matrix(matrix)
+	{
+		location = glGetUniformLocation(program, name);
+	}
+	void set(AbstractPresence * b)
+	{
+		GLfloat matrix2[16];
+		Matrix4x4 objectMatrix;
+		b->getMatrix(objectMatrix.elements); // FIXME optimize!!
+		prod(objectMatrix.elements, matrix, matrix2);
+		glUniformMatrix4fv(location, 1, GL_FALSE, matrix2);
+	}
 };
 
 class UniformTexture
@@ -47,8 +67,14 @@ class UniformTexture
 private:
 	GLint location;
 public:
-	UniformTexture(GLuint program, const GLchar *name);
-	void set(AbstractPresence * b);
+	UniformTexture(GLuint program, const GLchar *name)
+	{
+		location = glGetUniformLocation(program, name);
+	}
+	void set(AbstractPresence * b)
+	{
+		glUniform1i(location, b->getFigure()->texture->id);
+	}
 };
 
 class VertexAttrib
@@ -61,15 +87,33 @@ private:
 	GLsizei stride;
 	const void * pointer;
 public:
-	VertexAttrib(GLuint program, GLuint index, const GLchar *name, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const void * pointer);
-	~VertexAttrib();
-	void set(AbstractPresence * b);
+	VertexAttrib(GLuint program, GLuint index, const GLchar *name, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const void * pointer) :
+	index(index), size(size), type(type), normalized(normalized), stride(stride), pointer(pointer)
+	{
+		glEnableVertexAttribArray(index);
+		glBindAttribLocation(program, index, name);
+	}
+	~VertexAttrib()
+	{
+		glDisableVertexAttribArray(index);
+	}
+	void set(AbstractPresence * b)
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[b->getFigure()->id]);
+		glVertexAttribPointer(index, size, type, normalized, stride, pointer);
+	}
 };
 
 class StencilOperation
 {
 public:
-	StencilOperation();
-	void set(AbstractPresence * b);
+	StencilOperation()
+	{
+	}
+	void set(AbstractPresence * b)
+	{
+		glStencilOp(GL_KEEP, GL_KEEP, b->label ? GL_REPLACE : GL_KEEP);
+		glStencilFunc(GL_ALWAYS, b->label, 0xFF);
+	}
 };
 
